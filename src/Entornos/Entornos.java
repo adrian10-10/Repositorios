e6c53a679187129fca54d074618421da5e9e67a5
adrix <adrix_99@hotmail.es>
2024-02-05 20:06:59 +0100

    Nuevo commit

--- src/Entornos/Entornos.java
+++ src/Entornos/Entornos.java
@@ -3,7 +3,8 @@ package Entornos;
 public class Entornos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		System.out.println("Mi nombre es Adrian");
 
 	}
 
